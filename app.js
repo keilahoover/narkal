cb0b292b10716c461080dac3a188bb91c73d4f8c
Keila Hoover <keila.hoover@gmail.com>
2018-06-02 19:02:47 -0600

    Add head, header, rest. profile and registration, and sign up views

--- app.js
+++ app.js
@@ -3,21 +3,21 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var handlebars = require('handlebars')
+// handlebars.registerPartial('head', '{{head}}')
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'hbs');
-// app.engine('html', require('hbs').__express);
-
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
